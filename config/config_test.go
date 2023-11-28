804d7588e3dd9dffd0186902625b273db155411b
Andres Puello <apuello1025@gmail.com>
2023-11-27 20:43:53 -0500

    fix: fix git bugs

--- config/config_test.go
+++ config/config_test.go
@@ -9,6 +9,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestEnvironments_WhenAllVarsAreSet(t *testing.T) {
+	config.Environments()
+
+	assert.Equal(t, "default", config.Cfg.Postgres.DbName)
+}
+
 func TestEnvironments_WhenNoVarsAreSet(t *testing.T) {
 	errExpected := "Error parsing environment vars &errors.errorString{s:\"Key: 'Config.Server.Port' Error:Field validation for 'Port' failed on the 'required' tag;\\nKey: 'Config.Postgres.Host' Error:Field validation for 'Host' failed on the 'required' tag;\\nKey: 'Config.Postgres.Port' Error:Field validation for 'Port' failed on the 'required' tag;\\nKey: 'Config.Postgres.User' Error:Field validation for 'User' failed on the 'required' tag;\\nKey: 'Config.Postgres.Password' Error:Field validation for 'Password' failed on the 'required' tag;\\nKey: 'Config.Postgres.DbName' Error:Field validation for 'DbName' failed on the 'required' tag\"}"
 	config.Once = sync.Once{}
@@ -22,10 +28,4 @@ func TestEnvironments_WhenNoVarsAreSet(t *testing.T) {
 	}()
 
 	config.Environments()
-}
-
-func TestEnvironments_WhenAllVarsAreSet(t *testing.T) {
-	config.Environments()
-
-	assert.Equal(t, "default", config.Cfg.Postgres.DbName)
 }
